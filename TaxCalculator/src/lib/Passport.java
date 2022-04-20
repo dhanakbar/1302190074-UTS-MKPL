55341d0e2b70a3ddb7278967083e8d4cc105bac8
dhanakbar <maulana261100@gmail.com>
2022-04-20 21:08:42 +0700

    Memecah method yang panjang menjadi method lain dan menghapus comments

--- TaxCalculator/src/lib/Passport.java
+++ TaxCalculator/src/lib/Passport.java
@@ -1,35 +1,21 @@
 package lib;
 public class Passport extends Status{
-	//Passport
 	private boolean isForeigner;
-	private enum Gender{LAKI_LAKI, PEREMPUAN}; //true = Laki-laki, false = Perempuan
+	private enum Gender{LAKI_LAKI, PEREMPUAN};
 	private String gender;
 
-
-    /**
-     * @return boolean return the isForeigner
-     */
     public boolean isIsForeigner() {
         return isForeigner;
     }
 
-    /**
-     * @param isForeigner the isForeigner to set
-     */
     public void setIsForeigner(boolean isForeigner) {
         this.isForeigner = isForeigner;
     }
 
-    /**
-     * @return String return the gender
-     */
     public String getGender() {
         return gender;
     }
 
-    /**
-     * @param gender the gender to set
-     */
     public void setGender(boolean gender) {
 		this.gender = gender? Gender.LAKI_LAKI.toString() : Gender.PEREMPUAN.toString();
     }
